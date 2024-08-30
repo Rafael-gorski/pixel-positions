26a54d099f8df61c95cd820c2e1c0df70ebbb60a
Rafael-gorski <rafaelgorski18@hotmail.com.br>
2024-08-30 18:43:46 -0300

    Creating the frontend

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,18 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo/>
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 font-bold text-xl transition-colors duration-150">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full time - from $60,000</p>
+    </div>
+
+    <div class="">
+        <x-tag size="small">Backend</x-tag>
+        <x-tag size="small">Frontend</x-tag>
+        <x-tag size="small">Manager</x-tag>
+    </div>
+</x-panel>
---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,17 @@
+<x-panel class="flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-600 font-bold text-xl transition-colors duration-150">Video Producer</h3>
+        <p class="text-sm mt-4">Full time - from $60,000</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag size="small">Backend</x-tag>
+            <x-tag size="small">Frontend</x-tag>
+            <x-tag size="small">Manager</x-tag>
+        </div>
+
+        <x-employer-logo :width="42"/>
+    </div>
+</x-panel>
