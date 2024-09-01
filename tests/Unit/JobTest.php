34061f06dc4fa8ddf0ef8eff84b4e8b19eb8a88b
Rafael-gorski <rafaelgorski18@hotmail.com.br>
2024-09-01 12:52:38 -0300

    Finishing factories, migrations and controllers

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,16 +1,19 @@
 <?php
 
-namespace Tests\Unit;
+use App\Models\Employer;
+use App\Models\Job;
 
-use PHPUnit\Framework\TestCase;
+it('belongs to an employer', function () {
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id
+    ]);
 
-class JobTest extends TestCase
-{
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+    expect($job->employer->is($employer))->toBeTrue();
+});
+
+it('can have tags', function () {
+    $job = Job::factory()->create();
+    $job->tag('Frontend');
+    expect($job->tags)->toHaveCount(1);
+});
