68191a1ed883227380454102c1f9875113370a2a
Rafael-gorski <rafaelgorski18@hotmail.com.br>
2024-09-01 15:45:08 -0300

    Finishing the architecture for the job platform

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,18 +1,20 @@
+@props(['job'])
+
 <x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo/>
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+        <a href="#" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 font-bold text-xl transition-colors duration-150">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full time - from $60,000</p>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 font-bold text-xl transition-colors duration-150">{{ $job->title }}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
     </div>
 
     <div class="">
-        <x-tag size="small">Backend</x-tag>
-        <x-tag size="small">Frontend</x-tag>
-        <x-tag size="small">Manager</x-tag>
+        @foreach($job->tags as $tag)
+            <x-tag :$tag />
+        @endforeach
     </div>
 </x-panel>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,15 +1,17 @@
+@props(['job'])
+
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Laracasts</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
     <div class="py-8">
-        <h3 class="group-hover:text-blue-600 font-bold text-xl transition-colors duration-150">Video Producer</h3>
-        <p class="text-sm mt-4">Full time - from $60,000</p>
+        <h3 class="group-hover:text-blue-600 font-bold text-xl transition-colors duration-150">{{ $job->title }}</h3>
+        <p class="text-sm mt-4">{{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag size="small">Backend</x-tag>
-            <x-tag size="small">Frontend</x-tag>
-            <x-tag size="small">Manager</x-tag>
+            @foreach($job->tags as $tag)
+                <x-tag :$tag size="small" />
+            @endforeach
         </div>
 
         <x-employer-logo :width="42"/>
