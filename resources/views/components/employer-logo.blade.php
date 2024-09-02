a2194c7682d4599b3520b393d430e068f8c70102
Rafael-gorski <rafaelgorski18@hotmail.com.br>
2024-09-02 19:43:19 -0300

    Some front-end and new search and job improvements

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,2 +1,2 @@
-@props(['width' => 90])
-<img src="http://picsum.photos/seed/{{ rand(0, 100000) }}/{{ $width }}" alt="" class="rounded-xl">
+@props(['employer', 'width' => 90])
+<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}">
